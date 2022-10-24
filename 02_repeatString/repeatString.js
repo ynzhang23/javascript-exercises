6e5d63ec776a3ffb13dea3aa009c0b36fbee48e2
Yining <ynzhang23@gmail.com>
2022-10-24 17:00:18 +0800

    Complete exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let output = "";
+    if (num < 0) {
+        return output = "ERROR";
+    }
+    for (i = 0; i < num; i++) {
+        output += string;
+    }
+    return output;
 };
 
 // Do not edit below this line
