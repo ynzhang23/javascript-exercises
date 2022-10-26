72a32e3fc31454127b67cd875b8745b56dd487b3
Yining <ynzhang23@gmail.com>
2022-10-26 09:35:52 +0800

    Complete exercise 4 and 5

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(...args) {
+    const array = args[0];
+    const newArray = [];
+    array.forEach((item) => {
+        if (!args.includes(item)) {
+            newArray.push(item)
+        }
+    });
+    return newArray;
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    if (typeof(num1) !== "number" ||
+        typeof(num2) !== "number" ||
+        num1 < 0 ||
+        num2 < 0) {
+        return "ERROR";
+    }
+    if (num1 > num2) {
+        [num1, num2] = [num2, num1];
+    }
+    let sum = num1;
+    for (i = 1; i < num2; i ++) {
+        if (num1 + i <= num2) {
+            sum += num1 + i;
+        }
+    }
+    return sum;
 };
 
 // Do not edit below this line
